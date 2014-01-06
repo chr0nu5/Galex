6cdb62363e26c6c1bff6772f7cf0787eb67676be
Victor Gama <hey@vito.io>
2014-01-06 21:46:54 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Galex::Application.config.secret_key_base = '248e6ccb5b3615cf83506edc994535f2f4db00e44082b6c2b5d51064315dc2d3b3bc4d81c71618a0871df2421ff320385779cf56a36ea3f8257ab4befbc298ad'
