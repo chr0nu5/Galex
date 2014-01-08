cc654d3a3dfce59fcacfa2feef66b1817ed017a0
Victor Gama <hey@vito.io>
2014-01-08 12:52:54 -0200

    Added views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   before_filter :require_auth
   
   def index
+    @documents = Document.all.order(:updated_at).reverse_order
+    @unread_documents = Document.where.not(id: current_user.read_documents.pluck(:id))
   end
 end
