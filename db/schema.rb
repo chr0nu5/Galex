71a3c54f3dceee21835fe1ea55c25481ce375a0a
Victor Gama <hey@vito.io>
2014-01-11 16:04:42 -0200

    Added 'format' column to Documents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108132546) do
+ActiveRecord::Schema.define(version: 20140111180223) do
 
   create_table "documents", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140108132546) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "format"
   end
 
   create_table "reads", force: true do |t|
