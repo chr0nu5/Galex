e6a9096660db9a17770096fce5ddf02af03c1062
Victor Gama <hey@vito.io>
2014-01-11 22:04:01 -0200

    Improved SQL performance on repository homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :require_auth
   
   def index
-    @documents = Document.all.order(:updated_at).reverse_order
-    @unread_documents = Document.where.not(id: current_user.read_documents.pluck(:id))
+    @documents = Document.all.includes(:user, :reads).order(:updated_at).reverse_order
+    @unread_documents = Document.includes(:user).where.not(id: current_user.read_documents.pluck(:id))
   end
 end
